a0e58b74f3f9c53ab4bdddb5fdf4f0b753e91f60
Vincent Macri <developer@vincemacri.ca>
2018-05-23 09:59:56 -0400

    Setup Respondent class for analysis.
    Former-commit-id: 3addbae4305d7dc217975da793febe6b932b1a46

---
+++ Analysis/Respondent.py
@@ -0,0 +1,6 @@
+class Respondent:
+
+	def __init__(self, grade, favouriteSubject, ethnicBackground):
+		self.grade = grade
+		self.favouriteSubject = favouriteSubject
+		self.ethnicBackground = ethnicBackground
