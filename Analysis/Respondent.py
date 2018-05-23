6bb59c193470a5c7c4ba57262774d83b024cf838
Vincent Macri <developer@vincemacri.ca>
2018-05-23 13:22:47 -0400

    Remove python file, using R instead.
    Former-commit-id: 9b8f5ca78cbf64b0758c3b33db3970dfd86c241c

--- Analysis/Respondent.py
+++
@@ -1,6 +0,0 @@
-class Respondent:
-
-	def __init__(self, grade, favouriteSubject, ethnicBackground):
-		self.grade = grade
-		self.favouriteSubject = favouriteSubject
-		self.ethnicBackground = ethnicBackground
