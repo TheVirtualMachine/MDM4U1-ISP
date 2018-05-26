6e0edd3a42fa60776afa6e81e005f6af18be9a55
Vincent Macri <developer@vincemacri.ca>
2018-05-26 13:45:29 -0400

    Improve graph.
    Former-commit-id: ea11cf01d49ed9644148e94f08eb54938be6b328

--- Analysis/PurityOrthodoxyPlot.R
+++ Analysis/PurityOrthodoxyPlot.R
@@ -1,15 +1,16 @@
 # This is a program to calculate and plot the orthodoxy score on the sandwich spectrum for all respondents.
 # Unless otherwise specified, throughout this file, "sandwich question" refers to a question about either sandwiches or ingredients.
 
-library(data.table)
+library(ggplot2)
 source("Functions.R")
 
+CATEGORY <- 4 # What column the categorical data is in.
 QUESTIONS_START <- 6 # What column the sandwich questions start at.
 QUESTIONS_END <- 48 # What column the sandwich questions end at.
 lockBinding("QUESTIONS_START", globalenv())
 lockBinding("QUESTIONS_END", globalenv())
 
-respondents <- as.data.table(read.csv("Responses.csv")) # Read in the CSV.
+respondents <- read.csv("Responses.csv") # Read in the CSV.
 NUM_RESPONDENTS <- nrow(respondents)
 lockBinding("NUM_RESPONDENTS", globalenv())
 
@@ -19,7 +20,6 @@ sandwichResponses <- 5 - sandwichResponses
 
 NUM_QUESTIONS <- nrow(sandwichResponses) # The number of sandwich questions.
 lockBinding("NUM_QUESTIONS", globalenv())
-
 
 totalResponse <- numeric(NUM_QUESTIONS)
 averageResponse <- numeric(NUM_QUESTIONS)
@@ -39,12 +39,33 @@ for (i in 1:NUM_RESPONDENTS) {
 }
 purityScores <- (purityScores - mean(purityScores)) / sd(purityScores)
 
-lo <- loess(orthodoxyScores ~ purityScores)
+# Output the data.
+data <- data.frame(purity = purityScores, orthodoxy = orthodoxyScores, cat = respondents[,CATEGORY])
 
-pdf("PurityVsOrthodoxy.pdf")
-plot(purityScores, orthodoxyScores, xlab="Purity", ylab="Orthodoxy", main="Purity vs Orthodoxy")
-lines(predict(lo), col="red", lwd=2)
-invisible(dev.off())
+print("Summaries:")
+length(which(data$cat == "Academic"))
+length(which(data$cat == "Gifted"))
+length(which(data$cat == "MaCS"))
+print("Purity")
+mean(data[which(data$cat == "Academic"),1])
+mean(data[which(data$cat == "Gifted"),1])
+mean(data[which(data$cat == "MaCS"),1])
+mean(data[,1])
+print("Orthodoxy")
+mean(data[which(data$cat == "Academic"),2])
+mean(data[which(data$cat == "Gifted"),2])
+mean(data[which(data$cat == "MaCS"),2])
+mean(data[,2])
+print("End of summaries.")
+
+# Create the plot.
+dataPlot <- ggplot(data, aes(purity, orthodoxy)) # Setup the plot.
+#dataPlot <- dataPlot + coord_cartesian(ylim = c(-1, 1)) # Set the graph limits.
+dataPlot <- dataPlot + geom_point(aes(colour = cat)) # Add the data points.
+dataPlot <- dataPlot + geom_smooth(method = loess, level = 0.95, colour = "black", size = 0.5) # Add the confidence curve.
+dataPlot <- dataPlot + geom_smooth(method = lm, se = FALSE, colour = "black", size = 0.5, linetype="dashed") # Add the line of best fit.
+dataPlot <- dataPlot + labs(x = "Purity", y = "Orthodoxy", colour = "Academic Stream") # Give axes and legend proper labels.
+ggsave("PurityVsOrthodoxy.pdf", plot=dataPlot, width=9, height=8)
 
 results <- rbind(purityScores, orthodoxyScores)
 write.csv(results, "Results.csv")
