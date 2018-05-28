7b71a0f91ca74d5db68ad09ba8929e675dbca340
Vincent Macri <developer@vincemacri.ca>
2018-05-28 09:54:16 -0400

    Create box plots on more categorical variables.
    Former-commit-id: e652d0f1f1a10289b93a5b8c494fc3b2d4251030

--- Analysis/BoxPlots.R
+++ Analysis/BoxPlots.R
@@ -1,21 +1,26 @@
-# This is a program to calculate and plot the orthodoxy score on the sandwich spectrum for all respondents.
-# Unless otherwise specified, throughout this file, "sandwich question" refers to a question about either sandwiches or ingredients.
+# This program creates box plots of the respondents broken down by their categorical data.
 
 library(ggplot2)
 source("Calculate.R")
 
-data <- data[data$stream %in% names(table(data$stream))[table(data$stream) > 1],] # Remove all streams only occurring once, as these data are not helpful for a box plot.
+makeBoxPlot <- function(categoryData, categoryName, categoryTitle) {
+	boxPlotData <- data[categoryData %in% names(table(categoryData))[table(categoryData) > 1],] # Remove all categorical data points only occurring once, as these data are not helpful for a box plot.
 
-# Create the purity plot.
-dataPlot <- ggplot(data, aes(stream, purity, fill=stream), ) # Setup the plot.
-dataPlot <- dataPlot + geom_boxplot() # Add the data points.
-dataPlot <- dataPlot + labs(x = "Academic Stream", y = "Purity") # Give axes and legend proper labels.
-dataPlot <- dataPlot + theme(legend.position = "none")
-ggsave("StreamsPurity.pdf", plot=dataPlot, width=9, height=8)
+	# Create the purity plot.
+	dataPlot <- ggplot(boxPlotData, aes_string(categoryName, "purity", fill=categoryName), ) # Setup the plot.
+	dataPlot <- dataPlot + geom_boxplot() # Add the data points.
+	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Purity") # Give axes and legend proper labels.
+	dataPlot <- dataPlot + theme(legend.position = "none")
+	ggsave(paste(categoryTitle, "Purity.pdf"), plot=dataPlot, width=9, height=8)
 
-# Create the orthodoxy plot.
-dataPlot <- ggplot(data, aes(stream, orthodoxy, fill=stream), ) # Setup the plot.
-dataPlot <- dataPlot + geom_boxplot() # Add the data points.
-dataPlot <- dataPlot + labs(x = "Academic Stream", y = "Orthodoxy") # Give axes and legend proper labels.
-dataPlot <- dataPlot + theme(legend.position = "none")
-ggsave("StreamsOrthodoxy.pdf", plot=dataPlot, width=9, height=8)
+	# Create the orthodoxy plot.
+	dataPlot <- ggplot(boxPlotData, aes_string(categoryName, "orthodoxy", fill=categoryName), ) # Setup the plot.
+	dataPlot <- dataPlot + geom_boxplot() # Add the data points.
+	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Orthodoxy") # Give axes and legend proper labels.
+	dataPlot <- dataPlot + theme(legend.position = "none")
+	ggsave(paste(categoryTitle, "Orthodoxy.pdf"), plot=dataPlot, width=9, height=8)
+}
+
+makeBoxPlot(data$grade, "grade", "Grade")
+makeBoxPlot(data$background, "background", "Ethnic Background")
+makeBoxPlot(data$stream, "stream", "Stream")
