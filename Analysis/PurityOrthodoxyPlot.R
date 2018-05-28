14329cfef5c8eeafc8ab99a6dbea16ff3c1230c7
Vincent Macri <developer@vincemacri.ca>
2018-05-28 13:22:01 -0400

    Modularize scatter plot code.
    Former-commit-id: cf887a8845e8575835cbb39f5e119040012d9bed

--- Analysis/PurityOrthodoxyPlot.R
+++ Analysis/PurityOrthodoxyPlot.R
@@ -4,14 +4,16 @@
 library(ggplot2)
 source("Calculate.R")
 
-# Create the plot.
-dataPlot <- ggplot(data, aes(purity, orthodoxy)) # Setup the plot.
-#dataPlot <- dataPlot + coord_cartesian(ylim = c(-1, 1)) # Set the graph limits.
-dataPlot <- dataPlot + geom_point(aes(colour = stream)) # Add the data points.
-dataPlot <- dataPlot + geom_smooth(method = loess, level = 0.95, colour = "black", size = 0.5) # Add the confidence curve.
-dataPlot <- dataPlot + geom_smooth(method = lm, se = FALSE, colour = "black", size = 0.5, linetype="dashed") # Add the line of best fit.
-dataPlot <- dataPlot + labs(x = "Purity", y = "Orthodoxy", colour = "Academic Stream") # Give axes and legend proper labels.
-ggsave("PurityVsOrthodoxy.pdf", plot=dataPlot, width=9, height=8)
+makeScatterPlot <- function(categoryName, categoryTitle) {
+	# Create the plot.
+	dataPlot <- ggplot(data, aes(purity, orthodoxy)) # Setup the plot.
+	#dataPlot <- dataPlot + coord_cartesian(ylim = c(-1, 1)) # Set the graph limits.
+	dataPlot <- dataPlot + geom_point(aes_string(colour = categoryName)) # Add the data points.
+	dataPlot <- dataPlot + geom_smooth(method = loess, level = 0.95, colour = "black", size = 0.5) # Add the confidence curve.
+	dataPlot <- dataPlot + geom_smooth(method = lm, se = FALSE, colour = "black", size = 0.5, linetype="dashed") # Add the line of best fit.
+	dataPlot <- dataPlot + labs(x = "Purity", y = "Orthodoxy", colour = categoryTitle) # Give axes and legend proper labels.
+	ggsave(paste(categoryTitle, "PurityVsOrthodoxy.pdf"), plot=dataPlot, width=9, height=8)
+}
 
-#results <- rbind(purityScores, orthodoxyScores)
-#write.csv(results, "Results.csv")
+makeScatterPlot("stream", "Stream")
+makeScatterPlot("grade", "Grade")
