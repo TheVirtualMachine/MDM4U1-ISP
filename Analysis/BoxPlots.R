ef24b61cfd45d6515c50748194275eb3c62b04b0
Vincent Macri <developer@vincemacri.ca>
2018-05-31 09:59:38 -0400

    Add bar plots of categorical data.

--- Analysis/BoxPlots.R
+++ Analysis/BoxPlots.R
@@ -9,15 +9,15 @@ makeBoxPlot <- function(categoryData, categoryName, categoryTitle) {
 	# Create the purity plot.
 	dataPlot <- ggplot(boxPlotData, aes_string(categoryName, "purity", fill=categoryName), ) # Setup the plot.
 	dataPlot <- dataPlot + geom_boxplot() # Add the data points.
-	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Purity") # Give axes and legend proper labels.
-	dataPlot <- dataPlot + theme(legend.position = "none")
+	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Purity") # Give axes proper labels.
+	dataPlot <- dataPlot + theme(legend.position = "none") # Remove the legend.
 	ggsave(paste(categoryTitle, "Purity.pdf"), plot=dataPlot, width=9, height=8)
 
 	# Create the orthodoxy plot.
 	dataPlot <- ggplot(boxPlotData, aes_string(categoryName, "orthodoxy", fill=categoryName), ) # Setup the plot.
 	dataPlot <- dataPlot + geom_boxplot() # Add the data points.
-	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Orthodoxy") # Give axes and legend proper labels.
-	dataPlot <- dataPlot + theme(legend.position = "none")
+	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Orthodoxy") # Give axes proper labels.
+	dataPlot <- dataPlot + theme(legend.position = "none") # Remove the legend.
 	ggsave(paste(categoryTitle, "Orthodoxy.pdf"), plot=dataPlot, width=9, height=8)
 }
 
---
+++ Analysis/Demographics.R
@@ -0,0 +1,19 @@
+# This program creates bar plots of the respondents broken down by their categorical data.
+
+library(ggplot2)
+source("Calculate.R")
+
+makeBarPlot <- function(categoryData, categoryName, categoryTitle) {
+	barPlotData <- data[categoryData %in% names(table(categoryData))[table(categoryData) > 1],] # Remove all categorical data points only occurring once, as these data are not helpful for a bar plot.
+
+	# Create the bar plot.
+	dataPlot <- ggplot(barPlotData, aes_string(categoryName, fill=categoryName), ) # Setup the plot.
+	dataPlot <- dataPlot + geom_bar() # Add the bar plot.
+	dataPlot <- dataPlot + labs(x = categoryTitle, y = "Count") # Give axes proper labels.
+	dataPlot <- dataPlot + theme(legend.position = "none") # Remove the legend.
+	ggsave(paste(categoryTitle, "BarPlot.pdf"), plot=dataPlot, width=9, height=8)
+}
+
+makeBarPlot(data$grade, "grade", "Grade")
+makeBarPlot(data$background, "background", "Ethnic Background")
+makeBarPlot(data$stream, "stream", "Stream")
