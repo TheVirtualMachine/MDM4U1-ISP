241523e0c0065a232c4f649b1062025f0511eb86
Vincent Macri <developer@vincemacri.ca>
2018-06-01 20:04:26 -0400

    Better demographic separation.

---
+++ Analysis/Levels.R
@@ -0,0 +1,31 @@
+# Output the levels for certain columns of interest in the input.
+
+# Define the constants for what columns data is in inside the CSV file.
+GRADE_COLUMN <- 1 # What column the grade data is in.
+SUBJECTS_COLUMN <- 2 # What column the favourite subjects data is in.
+BACKGROUND_COLUMN <- 3 # What column the ethnic background data is in.
+STREAM_COLUMN <- 4 # What column the stream data is in.
+QUESTIONS_START <- 6 # What column the food questions start at.
+QUESTIONS_END <- 48 # What column the food questions end at.
+lockBinding("GRADE_COLUMN", globalenv())
+lockBinding("SUBJECTS_COLUMN", globalenv())
+lockBinding("BACKGROUND_COLUMN", globalenv())
+lockBinding("STREAM_COLUMN", globalenv())
+lockBinding("QUESTIONS_START", globalenv())
+lockBinding("QUESTIONS_END", globalenv())
+
+respondents <- read.csv("CleanedData.csv") # Read in the CSV.
+
+print(paste(nrow(respondents), "respondents"))
+
+summary(respondents[,GRADE_COLUMN])
+print(paste(length(levels(respondents[,GRADE_COLUMN])), "grades"))
+
+#summary(respondents[,SUBJECTS_COLUMN])
+#print(paste(length(levels(respondents[,SUBJECTS_COLUMN])), "subjects."))
+
+summary(respondents[,BACKGROUND_COLUMN])
+print(paste(length(levels(respondents[,BACKGROUND_COLUMN])), "ethnic backgrounds"))
+
+summary(respondents[,STREAM_COLUMN])
+print(paste(length(levels(respondents[,STREAM_COLUMN])), "streams"))
--- Analysis/Residuals.R
+++ Analysis/Residuals.R
@@ -1,4 +1,4 @@
-# This program creates a residual plot of the respondents broken down by their categorical data.
+# This program creates a residual plot of the respondents.
 
 library(ggplot2)
 library(broom)
@@ -8,10 +8,19 @@ source("Calculate.R")
 residualPlotData <- data
 
 # Create the linear model.
+mod <- lm(orthodoxy ~ purity, data = residualPlotData)
+df <- augment(mod)
+
+# Create the residual plot.
+dataPlot <- ggplot(df, aes(.fitted, .resid)) + geom_point()
+dataPlot <- dataPlot + labs(x = "Fitted Value", y = "Residual") # Give axes proper labels.
+ggsave("LinearResidualPlot.pdf", plot=dataPlot, width=9, height=8)
+
+# Create the loess model.
 mod <- loess(orthodoxy ~ purity, data = residualPlotData)
 df <- augment(mod)
 
 # Create the residual plot.
 dataPlot <- ggplot(df, aes(.fitted, .resid)) + geom_point()
 dataPlot <- dataPlot + labs(x = "Fitted Value", y = "Residual") # Give axes proper labels.
-ggsave("ResidualPlot.pdf", plot=dataPlot, width=9, height=8)
+ggsave("LoessResidualPlot.pdf", plot=dataPlot, width=9, height=8)
