bf9b34d8dc13ffbd0798f1be0cfec7a247123bbc
Vincent Macri <developer@vincemacri.ca>
2018-06-02 18:39:00 -0400

    Round tables.

---
+++ Analysis/Tables.R
@@ -0,0 +1,7 @@
+# This program creates a CSV file with the data on each respondent's metrics.
+
+source("Calculate.R")
+
+data$purity <- round(data$purity, digits=5)
+data$orthodoxy <- round(data$orthodoxy, digits=5)
+write.csv(data, file = "Results.csv")
